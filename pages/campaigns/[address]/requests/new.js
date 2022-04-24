7eb79bd5ed5850462ec38377449b30b500efa8b0
bamblebam <burhanizrangwala@gmail.com>
2022-04-24 13:49:48 +0530

    create new request page done

--- pages/campaigns/[address]/requests/new.js
+++ pages/campaigns/[address]/requests/new.js
@@ -4,6 +4,8 @@ import Campaign from "../../../../ethereum/campaign";
 import web3 from "../../../../ethereum/web3";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Navbar from "../../../../components/Navbar";
+import styles from "../../../../styles/CampaignForm.module.css";
 import Layout from "../../../../components/Layout";
 
 const RequestNew = ({ address }) => {
@@ -44,42 +46,66 @@ const RequestNew = ({ address }) => {
   };
 
   return (
-    <Layout>
-      <Link href={`/campaigns/${address}/requests`}>
-        <a>Back</a>
-      </Link>
-      <h3>Create a Request</h3>
-      <Form onSubmit={onSubmit} error={!!errorMessage}>
-        <Form.Field>
-          <label>Description</label>
-          <Input
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
-          />
-        </Form.Field>
-
-        <Form.Field>
-          <label>Value in Ether</label>
-          <Input
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-          />
-        </Form.Field>
-
-        <Form.Field>
-          <label>Recipient</label>
-          <Input
-            value={recipient}
-            onChange={(event) => setRecipient(event.target.value)}
-          />
-        </Form.Field>
-
-        <Message error header="Oops!" content={errorMessage} />
-        <Button primary loading={loading}>
-          Create!
-        </Button>
-      </Form>
-    </Layout>
+    <>
+      <Navbar></Navbar>
+      <div className={styles.wrapper_form}>
+        <div className={styles.title}>
+          <h1 className={styles.heading}>New Request</h1>
+        </div>
+        {/* <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} />
+        <span className={styles.circle} /> */}
+        <form onSubmit={onSubmit}>
+          <div className={styles.card_glass}>
+            {/* <div className={styles.card_glass_header}>
+              <div className={styles.glass_header_text}>New Campaign</div>
+            </div> */}
+            <div className={styles.glass_container}>
+              <div className={styles.form_glass_group}>
+                <input
+                  type="text"
+                  className={styles.form_glass_control}
+                  value={description}
+                  onChange={(event) => setDescription(event.target.value)}
+                  placeholder="Request Description"
+                />
+              </div>
+              <div className={styles.form_glass_group}>
+                <input
+                  type="text"
+                  className={styles.form_glass_control}
+                  value={value}
+                  onChange={(event) => setValue(event.target.value)}
+                  placeholder="Request Amount"
+                />
+              </div>
+              <div className={styles.form_glass_group}>
+                <input
+                  type="text"
+                  className={styles.form_glass_control}
+                  value={recipient}
+                  onChange={(event) => setRecipient(event.target.value)}
+                  placeholder="Recipient"
+                />
+              </div>
+              {errorMessage && (
+                <Message error header="Oops!" content={errorMessage} />
+              )}
+              <div className={styles.buttonContainer}>
+                <button className={styles.btn}>Create Request</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
@@ -90,3 +116,40 @@ RequestNew.getInitialProps = ({ query }) => {
 };
 
 export default RequestNew;
+
+/* <Layout>
+<Link href={`/campaigns/${address}/requests`}>
+  <a>Back</a>
+</Link>
+<h3>Create a Request</h3>
+<Form onSubmit={onSubmit} error={!!errorMessage}>
+  <Form.Field>
+    <label>Description</label>
+    <Input
+      value={description}
+      onChange={(event) => setDescription(event.target.value)}
+    />
+  </Form.Field>
+
+  <Form.Field>
+    <label>Value in Ether</label>
+    <Input
+      value={value}
+      onChange={(event) => setValue(event.target.value)}
+    />
+  </Form.Field>
+
+  <Form.Field>
+    <label>Recipient</label>
+    <Input
+      value={recipient}
+      onChange={(event) => setRecipient(event.target.value)}
+    />
+  </Form.Field>
+
+  <Message error header="Oops!" content={errorMessage} />
+  <Button primary loading={loading}>
+    Create!
+  </Button>
+</Form>
+</Layout> */
